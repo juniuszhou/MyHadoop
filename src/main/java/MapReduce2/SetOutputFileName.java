bbdf75900a944ab4a92e4eda51a58131317b95a6
juniuszhou <junius.zhou@gmail.com>
2016-01-06 10:24:44 +0800

    add

--- src/main/java/MapReduce2/SetOutputFileName.java
+++ src/main/java/MapReduce2/SetOutputFileName.java
@@ -1,21 +1,23 @@
 package MapReduce2;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
+        import org.apache.hadoop.conf.Configuration;
+        import org.apache.hadoop.fs.Path;
+        import org.apache.hadoop.io.IntWritable;
+        import org.apache.hadoop.io.Text;
+        import org.apache.hadoop.mapred.lib.MultipleSequenceFileOutputFormat;
+        import org.apache.hadoop.mapreduce.Job;
+        import org.apache.hadoop.mapreduce.Mapper;
+        import org.apache.hadoop.mapreduce.Reducer;
+        import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+        import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+        import org.apache.hadoop.util.GenericOptionsParser;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.lib.MultipleSequenceFileOutputFormat;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+        import java.io.IOException;
+        import java.util.StringTokenizer;
 
-public class NewWordCount {
+public class SetOutputFileName {
 
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
@@ -50,14 +52,17 @@ public class NewWordCount {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        /*
+
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         if (otherArgs.length != 2) {
             System.err.println("Usage: wordcount <in> <out>");
             System.exit(2);
         }
-        */
+
+
+
         Job job = Job.getInstance(conf);
+        //set class for each task.
         job.setJarByClass(NewWordCount.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
@@ -65,14 +70,22 @@ public class NewWordCount {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
+        // Set number of map and reduce
+        // job.setNumMapTasks(2); // can't set now it depends on split
+
+        job.setNumReduceTasks(1);
+
         String inputPath = "E:\\input.txt";
         String outputPath = "E:\\";
+
         FileInputFormat.addInputPath(job, new Path(inputPath));
         //FileOutputFormat.setOutputPath(job, new Path(outputPath));
         TextOutputFormat.setOutputPath(job, new Path(outputPath));
         // MultipleSequenceFileOutputFormat.
+        MultipleSequenceFileOutputFormat.setCompressOutput();
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
+
 
 
